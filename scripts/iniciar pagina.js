02db78d15f068b1c7e08bc9d53a00d5dec5a675b
Joelisson Santana <Joelisson997@gmail.com>
2023-05-27 21:12:39 -0300

    Squashed commit of the following:
    commit 1a4b5437326b74a3953582911363bbb201c0882d
    Author: Joelisson Santana <Joelisson997@gmail.com>
    Date:   Thu May 18 20:48:33 2023 -0300
    
        .
    
    commit 32b1e3e97b6e8073b0e57bc0cff08afae637b87a
    Author: Joelisson Santana <Joelisson997@gmail.com>
    Date:   Thu May 18 20:37:41 2023 -0300
    
        adicionado sistema de link interno para direcionar
    
    commit 75181103d57cd7010a1aeafcaa91af3fe41722a4
    Author: Joelisson Santana <Joelisson997@gmail.com>
    Date:   Thu May 18 15:06:28 2023 -0300
    
        corrigido a responsividade da area resumo
    
    commit 1427c365dc407b4e66b6de6cc4fbe11c05e5c689
    Author: Joelisson Santana <Joelisson997@gmail.com>
    Date:   Thu May 18 15:00:53 2023 -0300
    
        correcao de bugs na adicao da responsividade dos projetos
    
    commit 987f0adf2ce22996d58fd3cc10472a80cfac69a6
    Author: Joelisson Santana <Joelisson997@gmail.com>
    Date:   Thu May 18 14:44:13 2023 -0300
    
        adicionando responsividade na area dos projetos
    
    commit f1e9853f70a24f2d918ca99b90e9752697c6bb21
    Author: Joelisson Santana <Joelisson997@gmail.com>
    Date:   Wed May 17 17:14:13 2023 -0300
    
        Update iniciar pagina.js
    
    commit 7c7ab82c930365259db3d6d8e0d4106e16da4dad
    Author: Joelisson Santana <Joelisson997@gmail.com>
    Date:   Wed May 17 17:05:47 2023 -0300
    
        inclemento do card projeto
    
    commit 5041210b6a633e10b4e9d101ade70a18f0cb2192
    Author: JoelissonSS <113321562+JoelissonSS@users.noreply.github.com>
    Date:   Tue May 16 18:41:28 2023 -0300
    
        commit inicial

---
+++ scripts/iniciar pagina.js
@@ -0,0 +1,10 @@
+function setarMain(atual='Resumo', liAtual='resumo'){
+    let li = document.getElementById(liAtual)
+    document.querySelectorAll('.nav-links').forEach(el => 
+        el.style.cssText = 'background-color: #2099d9;')
+    li.style.cssText = 'background-color: #E6AB73'
+    
+    let parag = document.getElementById(atual)
+    document.querySelectorAll('.pa').forEach(el => el.style.display = 'none')
+    parag.style.cssText = ''
+}
